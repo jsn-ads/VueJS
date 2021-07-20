9cf94dc95441e12a3fb4d0c9707a4da465c54aaa
José <jsn.ads@gmail.com>
2021-07-20 17:45:36 -0300

    01-06 Concluido

---
+++ VUE UDEMY/01-Dominando.os.principios/06-Diretivas.V-ON/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>V-ON</title>
+</head>
+<body>
+
+    <div id="app">
+        <button v-on:click="ff">Clique</button>
+        <br><br>
+        <p> qtd cliques : {{c}}</p>
+    </div>
+
+
+
+    <script src="../../FrameWork/vue.js"></script>
+
+    <script type="text/javascript">
+        
+        new Vue({
+            el:'#app',
+            data: {
+                c: 0
+            }, 
+            methods: {
+                ff : function(){
+                    this.c++;
+                }   
+            }
+        });
+
+    </script>
+</body>
+</html>
