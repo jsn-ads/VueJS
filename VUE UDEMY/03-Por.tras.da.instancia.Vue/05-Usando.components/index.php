c4d9823b7e83b084979a569fa32c8b43e7a1d3f1
José <jsn.ads@gmail.com>
2021-08-02 15:13:08 -0300

    03-05 Concluido

---
+++ VUE UDEMY/03-Por.tras.da.instancia.Vue/05-Usando.components/index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Usando Componentes</title>
+    <script src="../../FrameWork/vue.js"></script>
+</head>
+<body>
+
+<div id="container">
+    <minha-div></minha-div>
+</div>
+
+<script type="text/javascript">
+
+    <!-- primeiro cria componente e depois chama esse componente na instancia Vue -->
+
+    Vue.component('minha-div', {
+        template: "<h3>O componente criado por JSN</h3>"
+    });
+
+    var vm = new Vue({
+        el:"#container"
+    });
+
+
+</script>
+</body>
+</html>
