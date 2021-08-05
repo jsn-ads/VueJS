07384bcd22845933f884b22692b032a4bb2d9c28
José <jsn.ads@gmail.com>
2021-08-05 19:33:10 -0300

    06-01 Props e events

---
+++ VUE UDEMY/06-Props.e.Events.comunicação.entre.Components/jsnapp/src/components/FilmesLista.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="row">
+
+    <!-- coluna 1 -->
+    <div class="col-8">
+
+      <h2>Filmes</h2>
+
+      <ul class="list-group list-group-flush">
+
+        <FilmesListaIten v-for="filme in lista" :key="filme.id" v-bind="filme"/>
+       
+
+      </ul>
+    </div>
+
+    <!-- coluna 2 -->
+    <div class="col-4">
+
+      <FilmesListaItenInfo/>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+import FilmesListaIten from './FilmesListaIten.vue'
+import FilmesListaItenInfo from './FilmesListaItenInfo.vue'
+
+export default {
+  components: {
+    FilmesListaIten,
+    FilmesListaItenInfo
+  },
+  data() {
+    return {
+      lista:[
+        {id:1, titulo:"O Vingadores", ano:2018},
+        {id:2, titulo:"O Irlandes", ano:2019},
+        {id:3, titulo:"CHELSEA CHAMPIONS", ano:2021},
+        {id:4, titulo:"VUE DEIXA LOUCO", ano:2021},
+        {id:5, titulo:"BATMAN", ano:2009},
+      ]
+    }
+  },
+}
+</script>
---
+++ VUE UDEMY/06-Props.e.Events.comunicação.entre.Components/jsnapp/src/components/FilmesListaIten.vue
@@ -0,0 +1,21 @@
+<template>
+    <li class="list-group-item">
+        <span>{{titulo}} | {{ano}}</span>
+        <button class="btn btn-success float-right">Editar</button>
+    </li>
+</template>
+
+<script>
+export default {
+    props:{
+        titulo:{
+            type: String,
+            required: true
+        },
+        ano:{
+            type: Number,
+            required: true
+        }
+    }
+}
+</script>
