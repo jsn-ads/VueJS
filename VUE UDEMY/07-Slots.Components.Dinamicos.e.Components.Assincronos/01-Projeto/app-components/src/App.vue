e67330c23cbcf785d37c0b3fbad663ec907f144d
Neto <jsn.ads@gmail.com>
2021-08-10 00:55:30 -0300

    Alterado configuração do projeto

--- VUE UDEMY/07-Slots.Components.Dinamicos.e.Components.Assincronos/01-Projeto/app-components/src/App.vue
+++ VUE UDEMY/07-Slots.Components.Dinamicos.e.Components.Assincronos/01-Projeto/app-components/src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+      <h1>Curso de Vue</h1>
+      <Post :titulo="'Bem Vindo'"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Post from './components/Post.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components:{
+        Post
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
