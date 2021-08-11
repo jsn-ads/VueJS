d9be1e775af40317ad0dd5ec803524d0ff5613d9
Neto <jsn.ads@gmail.com>
2021-08-10 22:17:53 -0300

    mod

--- VUE UDEMY/07-Slots.Components.Dinamicos.e.Components.Assincronos/01-Projeto/app-components/src/App.vue
+++ VUE UDEMY/07-Slots.Components.Dinamicos.e.Components.Assincronos/01-Projeto/app-components/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-      <h1>Curso de Vue</h1>
-      <Post :titulo="'Bem Vindo'"/>
+    <h1>Curso de Vue</h1>
+    <Post :titulo="'Bem Vindo'" />
   </div>
 </template>
 
 <script>
-
-import Post from './components/Post.vue'
+import Post from "./components/Post.vue";
 
 export default {
-    components:{
-        Post
-    }
-}
+  components: {
+    Post,
+  },
+};
 </script>
