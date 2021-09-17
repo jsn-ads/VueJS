bd72bf2822b78a1bc2029fc3fdb32d081b3b2731
José <jsn.ads@gmail.com>
2021-09-17 14:24:43 -0300

    Comunicando com servidores HTTP usando Axios concluido

--- VUE UDEMY/11-Comunicando.com.servidores.HTTP.usando.Axios/http_vue/src/App.vue
+++ VUE UDEMY/11-Comunicando.com.servidores.HTTP.usando.Axios/http_vue/src/App.vue
@@ -12,6 +12,30 @@
     
       <div class="container">
           <TarefaLista></TarefaLista>
+
+          <button
+            class="btn btn-primary mt-4 mb-2"
+            @click="download" 
+          >
+            Baixar Imagem    
+          </button>
+        
+            <div class="progress">
+                <div
+                    class="progress-bar"
+                    role="progressbar"
+                    :style="{width: progresso + '%'}"
+                    :aria-valuenow="progresso"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                >
+                    {{progresso}}    
+                </div>
+            </div>
+
+            <div v-if="imagem">
+                <img :src="imagem" style="max-width: 100%">
+            </div>
       </div>
 
   </div>
@@ -24,9 +48,34 @@
 
 import TarefaLista from './components/TarefaLista.vue';
 
+//importando axios e configuraçoes
+import axios from 'axios'
+
 export default {
     components:{
        TarefaLista
-    }
+    },
+    data(){
+        return{
+            progresso: 0,
+            imagem: undefined
+        }
+    },
+    methods: {
+        download(){
+            axios.get('https://images.unsplash.com/photo-1631523407536-c545fc12b068?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',{
+                responseType:'blob',
+                onDownloadProgess : (ProgressEvent) =>{
+                    this.progresso = (ProgressEvent.loaded / ProgressEvent.total * 100 ).toFixed(0)
+                }
+            }).then( response=>{
+                const reader = new window.FileReader()
+                reader.readAsDataURL(response.data)
+                reader.onload = () =>{
+                    this.imagem = reader.result
+                }
+            })
+        }
+    },
 }
 </script>
--- VUE UDEMY/11-Comunicando.com.servidores.HTTP.usando.Axios/http_vue/src/config/config.js
+++ VUE UDEMY/11-Comunicando.com.servidores.HTTP.usando.Axios/http_vue/src/config/config.js
@@ -1,3 +1,4 @@
 export default{
-    apiURL: "http://localhost:3000"
+    apiURL: "http://localhost:3000",
+    apiIMG: "http://localhost:8080"
 }
